9404ab02656029d323e34d07ee087b7dbc6c8591
fmnihal <fattahmahmudnihal@gmail.com>
2025-12-12 23:39:14 +0600

    server.js setup

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const multer = require('multer');
+const { MongoClient } = require('mongodb');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
